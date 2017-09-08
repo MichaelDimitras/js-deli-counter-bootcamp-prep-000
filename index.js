a99631b360c139b0a8ae4577add72582c56077a2
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 22:12:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function nowServing(arr){
   }
   else return "There is nobody waiting to be served!";
 }
+
+function currentLine(arr){
+  if (arr.length == 0){
+    return "The line is currently empty."
+  }
+  else{
+    var curr = "The line is currently: ";
+    for(var i = 0; i < arr.length; i++){
+      curr += `${i + 1} ${arr[i], }`
+    }
+    return curr.slice(0,-2)
+  }
+}
