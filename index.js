ae8650cf655c8b6f29dbe05537e3f78df4cdd050
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 22:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(arr){
   if (arr.length > 0){
     var name = arr[0];
     arr = arr.slice(1);
-    return `Currently serving ${name}`
+    return `Currently serving ${name}.`
   }
   else return "There is nobody waiting to be served!";
 }
