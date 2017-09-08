50b3f241837c46a525ac5cc937b6d1747a3ef5b1
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 22:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ function takeANumber(name){
   return [`Welcome, ${name}. You are number ${katzDeli.length} in line.`]
 }
 
-function nowServing(){
+function nowServing(arr){
 
-  if (katzDeli.length > 0){
-    var name = katzDeli[0];
-    katzDeli = katzDeli.slice(1);
+  if (arr.length > 0){
+    var name = arr[0];
+    arr = arr.slice(1);
     return `Currently serving ${name}`
   }
+  else return "There is nobody waiting to be served!";
 }
