13d30f9851553d91e4df2a65697b8bba693e4a18
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 22:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(name){
   katzDeli = [...katzDeli, name];
-  return [`Welcome, ${name}. You are number ${katzDeli.length} in line.`]
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
 function nowServing(arr){
